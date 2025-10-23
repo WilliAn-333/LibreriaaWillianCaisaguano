70ae575106b81ed7d2aac0431b9f7a41315406fb
WilliAn-333 <www.pegazo23@gmail.com>
2025-10-23 18:18:41 -0500

    creacion

---
+++ src/main/java/ec/edu/espoch/libreriacaisaguanowillian/LibreriaCaisaguanoWillian.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.libreriacaisaguanowillian;
+
+/**
+ *
+ * @author USER
+ */
+public class LibreriaCaisaguanoWillian {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
